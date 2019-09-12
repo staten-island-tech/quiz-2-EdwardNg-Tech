d79bbafc9fa318d6da6a6df6e389a2274e48ab81
EdwardNg-Tech <55099020+EdwardNg-Tech@users.noreply.github.com>
2019-09-12 11:29:22 -0400

    Arrays

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let xMike, yMike, zMike, xJohn, yJohn, zJohn, averageMike, averageJohn;
+/* let xMike, yMike, zMike, xJohn, yJohn, zJohn, averageMike, averageJohn;
 xMike = 116;
 yMike = 94;
 zMike = 123;
@@ -6,7 +6,11 @@ xJohn = 89;
 yJohn = 120;
 zJohn = 103;
 averageMike = (xMike + yMike + zMike) / 3;
-averageJohn = (xJohn + yJohn + zJohn) / 3;
+averageJohn = (xJohn + yJohn + zJohn) / 3; */
+let teamJohn = [89,120,103];
+let teamMike = [116,94,123];
+let averageJohn = (teamJohn[0] + teamJohn[1] + teamJohn[2]) / 3;
+let averageMike = (teamMike[0] + teamMike[1] + teamMike[2]) / 3;
 if (averageMike > averageJohn) {
     console.log(`Mike wins the basketball game with an average of ${averageMike} points.`);
 } else if (averageMike < averageJohn) {
